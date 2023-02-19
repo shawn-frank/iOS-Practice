daac76f35b4dc08e422f191129d5fb13f8e285ca
Shawn Frank <shawn@shawn.design>
2023-02-19 16:11:06 +1000

    configure scene delegate

---
+++ Combine/CombinePractice/CombinePractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CombinePractice
+//
+//  Created by Shawn Frank on 19/2/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+    
+
+}
+
