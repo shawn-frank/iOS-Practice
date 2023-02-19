c72f5035534f8b32cb5c915d5cd2c27abfffd099
Shawn Frank <shawn@shawn.design>
2023-02-20 09:30:27 +1000

    initialize unit test project

---
+++ Unit Tests/BudgetTrackPractice/BudgetTrackPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BudgetTrackPractice
+//
+//  Created by Shawn Frank on 20/2/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+
+
+}
+
