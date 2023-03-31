90d47269db18f276d7abc08916690e828a838bb5
Shawn Frank <shawn@shawn.design>
2023-03-31 14:44:51 +1000

    implement card flip animation using AnimatableModifier protocol

--- SwiftUI/Memorize/Memorize/Views/Cardify.swift
+++ SwiftUI/Memorize/Memorize/Views/Cardify.swift
@@ -7,14 +7,23 @@
 
 import SwiftUI
 
-struct Cardify: ViewModifier {
-    var isFaceUp: Bool
+struct Cardify: AnimatableModifier {
+    var rotation: Double
+    
+    var animatableData: Double {
+        get { rotation }
+        set { rotation = newValue }
+    }
+    
+    init(isFaceUp: Bool) {
+        rotation = isFaceUp ? 0 : 180
+    }
     
     func body(content: Content) -> some View {
         ZStack {
             let shape = RoundedRectangle(cornerRadius: DrawingConstants.cornerRadius)
             
-            if isFaceUp {
+            if rotation < 90 {
                 shape.foregroundColor(.white)
                 shape.strokeBorder(lineWidth: DrawingConstants.lineWidth).foregroundColor(.red)
             }
@@ -22,8 +31,9 @@ struct Cardify: ViewModifier {
                 shape.fill()
             }
             
-            content.opacity(isFaceUp ? 1 : 0)
+            content.opacity(rotation < 90 ? 1 : 0)
         }
+        .rotation3DEffect(Angle.degrees(rotation), axis: (0, 1, 0))
     }
     
     private struct DrawingConstants {
--- SwiftUI/Memorize/Memorize/Views/EmojiMemoryGameView.swift
+++ SwiftUI/Memorize/Memorize/Views/EmojiMemoryGameView.swift
@@ -27,7 +27,9 @@ struct EmojiMemoryGameView: View {
                 CardView(card: card)
                     .padding(4)
                     .onTapGesture {
-                        game.choose(card)
+                        withAnimation {
+                            game.choose(card)
+                        }
                     }
             }
             
