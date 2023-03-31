3cce96add0d159cb100cb0cc632bdf080a6860de
Shawn Frank <shawn@shawn.design>
2023-03-31 15:04:56 +1000

    implement custom view appear and disappear transitions

--- SwiftUI/Memorize/Memorize/Views/EmojiMemoryGameView.swift
+++ SwiftUI/Memorize/Memorize/Views/EmojiMemoryGameView.swift
@@ -10,6 +10,8 @@ import SwiftUI
 struct EmojiMemoryGameView: View {
     @ObservedObject var game: EmojiMemoryGame
     
+    @State private var dealt = Set<Int>()
+    
     var body: some View {
         VStack {
             gameBody
@@ -20,12 +22,13 @@ struct EmojiMemoryGameView: View {
     
     var gameBody: some View {
         AspectVGrid(items: game.cards, aspectRatio: 2/3) { card in
-            if card.isMatched && !card.isFaceUp {
+            if isUndealt(card) || (card.isMatched && !card.isFaceUp) {
                 Color.clear
             }
             else {
                 CardView(card: card)
                     .padding(4)
+                    .transition(AnyTransition.asymmetric(insertion: .scale, removal: .opacity))
                     .onTapGesture {
                         withAnimation {
                             game.choose(card)
@@ -33,6 +36,11 @@ struct EmojiMemoryGameView: View {
                     }
             }
             
+        }
+        .onAppear {
+            for card in game.cards {
+                deal(card)
+            }
         }
         .foregroundColor(.red)
     }
@@ -43,6 +51,14 @@ struct EmojiMemoryGameView: View {
                 game.suffle()
             }
         }
+    }
+    
+    private func deal(_ card: EmojiMemoryGame.Card) {
+        dealt.insert(card.id)
+    }
+    
+    private func isUndealt(_ card: EmojiMemoryGame.Card) -> Bool {
+        !dealt.contains(card.id)
     }
 }
 
