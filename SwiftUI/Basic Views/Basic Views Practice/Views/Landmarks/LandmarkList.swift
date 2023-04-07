c454965c1ccab4d0f28c6be77b9f9f52f12ab4aa
Shawn Frank <shawn@shawn.design>
2023-04-07 15:08:44 +1000

    bind model state to views and handle user input

---
+++ SwiftUI/Basic Views/Basic Views Practice/Views/Landmarks/LandmarkList.swift
@@ -0,0 +1,47 @@
+//
+//  LandmarkList.swift
+//  Basic Views Practice
+//
+//  Created by Shawn Frank on 25/3/2023.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    @EnvironmentObject var modelData: ModelData
+    @State private var showFavoritesOnly = false
+    
+    var filteredLandmarks: [Landmark] {
+        modelData.landmarks.filter { !showFavoritesOnly || $0.isFavorite }
+    }
+    
+    var body: some View {
+        NavigationView {
+            List {
+                
+                Toggle(isOn: $showFavoritesOnly) {
+                    Text("Favorites only")
+                }
+                
+                ForEach(filteredLandmarks) { landmark in
+                    NavigationLink {
+                        LandmarkDetail(landmark: landmark)
+                    } label: {
+                        LandmarkRow(landmark: landmark)
+                    }
+                }
+            }
+            .navigationTitle("Landmarks")
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        ForEach(["iPhone 14 Pro Max (iOS 16.2)", "iPhone SE (3rd generation)"], id: \.self) { deviceName in
+            LandmarkList()
+                .previewDevice(PreviewDevice(rawValue: deviceName))
+                .previewDisplayName(deviceName)
+        }
+    }
+}
