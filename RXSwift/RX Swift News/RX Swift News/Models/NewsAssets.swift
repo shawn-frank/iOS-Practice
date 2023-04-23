b6290a0004b9c062b55fd58bdd3e41d211a7c25b
Shawn Frank <shawn@shawn.design>
2023-04-23 16:37:08 +1000

    initialize pods with rxswift

---
+++ RXSwift/RX Swift News/RX Swift News/Models/NewsAssets.swift
@@ -0,0 +1,8 @@
+//
+//  NewsAssets.swift
+//  RX Swift News
+//
+//  Created by Shawn Frank on 23/4/2023.
+//
+
+import Foundation
---
+++ RXSwift/RX Swift News/RX Swift News/Models/Response.swift
@@ -0,0 +1,12 @@
+//
+//  Response.swift
+//  RX Swift News
+//
+//  Created by Shawn Frank on 23/4/2023.
+//
+
+import Foundation
+
+struct Response {
+    let assets: [NewsAssets]
+}
