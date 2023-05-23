6662021677d99d558e7f49640bacef5328fa6cf1
Shawn Frank <shawn@shawn.design>
2023-05-23 21:43:22 +1000

    fix preview

--- SwiftUI/Basic Views/Basic Views Practice/Views/Landmarks/LandmarkList.swift
+++ SwiftUI/Basic Views/Basic Views Practice/Views/Landmarks/LandmarkList.swift
@@ -38,10 +38,12 @@ struct LandmarkList: View {
 
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
+        
         ForEach(["iPhone 14 Pro Max (iOS 16.2)", "iPhone SE (3rd generation)"], id: \.self) { deviceName in
             LandmarkList()
                 .previewDevice(PreviewDevice(rawValue: deviceName))
                 .previewDisplayName(deviceName)
+                .environmentObject(ModelData())
         }
     }
 }
